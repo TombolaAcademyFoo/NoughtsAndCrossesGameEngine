2c3a57a9d80153bfe9d64e26353887ad854601c0
keithbarrow <keith.barrow@tombola.com>
2014-10-02 20:21:40 +0100

    Got the non-virtual host part working.

--- app.js
+++ app.js
@@ -7,10 +7,25 @@ var logger = require('morgan');
 var app = new express();
 var config = require('./config');
 
-app.use(logger('dev'));
 if(config.api.useVHost) {
     app.use(vhost(config.api.vHostName, appFactory));
 }
+else {
+    app = appFactory;
+}
+
+app.use(logger('dev'));
+
+app.disable('etag');//prevent the http 304 (not modified) responses, less efficient but better for seeing what it going on.;
+
+//Allow CORS
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
+
+
 
 var server = app.listen(config.portNumber, function() {
     console.log('Express server listening on port ' + server.address().port);
